9b714a030cb999a5beeea37d0f8335310af96e79
bruce <bruce.mcintyre@gmail.com>
2020-11-09 21:46:32 +0000

    Adding this

--- eero/tests/eero/test_eero.py
+++ eero/tests/eero/test_eero.py
@@ -8,3 +8,12 @@ def test_eero():
     assert e is not None
     assert e._cookie_dict is not None
     assert e.needs_login()
+
+
+def test_id_from_url():
+    session = CookieStore("session.cookierandom")
+    e = eero.Eero(session)
+    result = e.id_from_url("/123")
+    assert result == "123"
+    result = e.id_from_url("123")
+    assert result == "123"
