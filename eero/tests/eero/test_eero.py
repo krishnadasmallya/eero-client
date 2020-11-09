a7f0f29c4ca8b2761e2191ee1605bfa31763ae28
bruce <bruce.mcintyre@gmail.com>
2020-11-09 23:39:02 +0000

    Adding this

--- eero/tests/eero/test_eero.py
+++ eero/tests/eero/test_eero.py
@@ -1,5 +1,6 @@
 import eero
 from eero.cookie_store import CookieStore
+import responses
 
 
 def test_eero():
@@ -17,3 +18,25 @@ def test_id_from_url():
     assert result == "123"
     result = e.id_from_url("123")
     assert result == "123"
+
+
+@responses.activate
+def test_eero_login():
+    session = CookieStore("session")
+    e = eero.Eero(session)
+    r = {"data": {"user_token": "test token"}, "meta": {"code": 200}}
+    responses.add(
+        responses.POST, "https://api-user.e2ro.com/2.2/login", json=r
+    )
+    assert "test token" == e.login("blah")
+
+
+@responses.activate
+def test_eero_login_verify():
+    session = CookieStore("session")
+    e = eero.Eero(session)
+    r = {"data": {"user_token": "test token"}, "meta": {"code": 200}}
+    responses.add(
+        responses.POST, "https://api-user.e2ro.com/2.2/login/verify", json=r
+    )
+    e.login_verify("code", "token")
